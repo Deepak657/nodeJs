4f8612fedb49af532352c88b3320dfe5d4009b8c
Deepak Kumar <dk6570049@gmail.com>
2023-06-29 01:37:52 +0530

    create a getTourStats and getMonthlyPlan controller for aggregation pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,8 @@ const {
   updateTour,
   getTour,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 } = require("./../controllers/tourController");
 
 const router = express.Router();
@@ -14,7 +16,8 @@ const router = express.Router();
 // router.param("id", checkID);
 
 router.route("/top-3-cheap").get(aliasTopTours, getAllTours);
-
+router.route("/tour-stats").get(getTourStats);
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 router.route("/").get(getAllTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
